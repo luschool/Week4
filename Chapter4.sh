83f5c584cc6c2daf3564230e05d1a16cfca47ca5
luschool <35786777+luschool@users.noreply.github.com>
2018-02-13 02:06:17 -0600

    Final Script

--- Chapter4.sh
+++ Chapter4.sh
@@ -1,1 +1,40 @@
- 
+#!/bin/bash
+#Week 4 Lab - Lucas Merriott
+echo First we will create an empty file 
+
+dd if=/dev/zero of=./24MB.img bs=1M count=24
+
+echo 
+echo next we will turn that empty file into a ext4 filesystem
+
+mkfs -t ext4 ./24MB.img
+
+echo now we will mount it to /mnt/tmp and verify with lsblk
+
+sudo mkdir /mnt/tmp
+sudo mount ./24MB.img /mnt/tmp
+echo
+
+lsblk
+
+sudo umount /mnt/tmp
+
+hexdump ./24MB.img > Hexdump.txt
+
+echo Hexdump of filesystem created in home directory
+echo Now to check if we properly unmounted
+echo
+
+
+#Clean up time
+cd /mnt
+
+sudo rmdir tmp
+
+cd
+
+rm 24MB.img
+
+#Verify that we unmounted
+lsblk
+echo all directories and mounts made by the script have been removed.
